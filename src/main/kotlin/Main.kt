b495d69ac2e276aafb7fc54cf09ee2fdb794b81d
ProcrastionationExpert <4zzyhy@gmail.com>
2025-10-21 13:28:32 +0700

    testing2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,66 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class Angka(val angka: Int) {
+    fun Int.tambah10(): Int {
+        return this + 10
     }
+
+    var baca: String
+        get() = "Angka ini dibaca"
+        set(angkas) {
+            println("abcdefhg $angkas")
+        }
+}
+
+open class Kendaraan(val merek: String) {
+    open fun info() {
+       println("Ini adalah kendaraan dengan merek $merek")
+    }
+}
+
+class Mobil(merek: String, val kecepatan: Int): Kendaraan(merek) {
+    override fun info() {
+        println("Ini adalah mobil dengan merek $merek yang memiliki kecepatan sebesar $kecepatan km/jam")
+    }
+    fun jalan() {
+        println("Mobil dengan merek $merek sedang berjalan")
+    }
+}
+
+val Int.isEven: Boolean
+    get() = this % 2 == 0
+
+fun main() {
+    val menuList = """
+        ========================================
+        Program manajemen menu restoran
+        ========================================
+    """.trimIndent()
+
+    println(menuList)
+
+    val mobilSaya = Mobil("Avanza", 999)
+    mobilSaya.info()
+    mobilSaya.jalan()
+
+    val kendaraanSiapa = Kendaraan("Esemka")
+    kendaraanSiapa.info()
+
+    val angkaSaya = Angka(999)
+    println(angkaSaya.baca)
+    angkaSaya.baca = "1230942908"
+
+    val angkarandom = 1999
+    println("Apakah angka random tersebut genap? ${angkarandom.isEven}")
+
+    fun String?.getLength(): Int {
+        return this?.length ?: 0
+    }
+
+    var abcde: String? = "asasaas"
+    abcde = null
+    println(abcde.getLength())
+
+
+
 }
---
+++ src/main/kotlin/mainMenu/Menu.kt
@@ -0,0 +1,5 @@
+package org.example.mainMenu
+
+fun main() {
+    println("Ini adalah menu program dari manajemen menu restoran!")
+}
